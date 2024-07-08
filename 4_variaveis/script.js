4298d1d8a5160adc771369f005b24c966af9df6f
Fernando Cruz <nando33_2005@hotmail.com>
2024-07-08 17:07:58 -0300

    Aula 4 variáveis
    Alua 4 sobre variáveis.

---
+++ 4_variaveis/script.js
@@ -0,0 +1,38 @@
+var teste = 1;
+console.log(teste)
+
+var meuNome;
+console.log(meuNome);
+
+meuNome = "Fernando Aguiar";
+console.log(meuNome);
+
+// nome = Lucas;  essa variável irá sobrescrever outra variável no escopo do software.
+
+teste = "Fernando";
+console.log(teste);
+
+// Não colocar caracteres especiais na frente do nome de variáveis.
+// Os únicos liberados são: $, # e _
+
+var $nome = "ADS";
+var _nome = "ADS1";
+
+console.log($nome);
+console.log(_nome);
+
+// Não colocar númaros na frente do nome de variáveis.
+
+var nome3 = 'ADS3';
+console.log(nome3);
+
+// Nome composto de variáveis deve ser CamelCase
+
+var meuPrimeiroNome = "Fernando";
+var meuSobrenome = "Cruz";
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
