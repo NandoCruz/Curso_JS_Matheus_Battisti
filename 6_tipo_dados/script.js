deeed223aaf78b71376e9c0e4b69ca989ef857a8
Fernando Cruz <nando33_2005@hotmail.com>
2024-07-11 16:48:06 -0300

    Aulas 5 e 6
    Adicionando aulas 5 e 6 de JavaScript

---
+++ 6_tipo_dados/script.js
@@ -0,0 +1,19 @@
+var nome = "Fernando";
+var sobrenome = 'Cruz';
+console.log(nome);
+console.log(sobrenome);
+console.log(typeof nome);
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+console.log(typeof "ads");
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase complexa';
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + ', ' + frase);
+
+document.write("Olá!" + ' ' + nome)
